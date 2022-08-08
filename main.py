a92c0d56556e4b44000a4ee0c452511abff4e7aa
Catalin <catalinsuceveanu@gmail.com>
2022-08-08 19:53:05 +0300

    TGP01 (refactor): rearrange the variables to make more sense

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import processor
 
-teams = {}
-
 
 def main():
-    no_of_teams_in_tournament = get_the_correct_input_for_no_of_teams()
-    for team in range(no_of_teams_in_tournament):
-        teams[get_the_correct_input_for_team_name()] = None
+    teams = {}
+    no_of_teams = get_the_correct_input_for_no_of_teams()
+    for team_no in range(no_of_teams):
+        teams[get_the_correct_input_for_team_name(team_no, teams)] = None
     no_of_games_played_during_season = (
-        get_the_correct_input_for_no_of_games_played_during_season()
+        get_the_correct_input_for_no_of_games_played_during_season(no_of_teams)
     )
     for team in teams:
         teams[team] = get_the_correct_input_for_no_of_wins_per_team(
             team, no_of_games_played_during_season
         )
+    games_to_be_played = []
 
 
 def get_the_correct_input_for_no_of_teams():
@@ -30,11 +30,11 @@ def get_the_correct_input_for_no_of_teams():
     return current_input
 
 
-def get_the_correct_input_for_team_name():
+def get_the_correct_input_for_team_name(team_no, teams):
     correct_input = False
     current_input = None
     while correct_input == False:
-        current_input = input(f"Enter the name of team {len(teams)+1}: ")
+        current_input = input(f"Enter the name of team {team_no + 1}: ")
         if len(current_input) >= 2 and current_input not in teams:
             correct_input = True
         elif current_input in teams:
@@ -46,12 +46,12 @@ def get_the_correct_input_for_team_name():
     return current_input
 
 
-def get_the_correct_input_for_no_of_games_played_during_season():
+def get_the_correct_input_for_no_of_games_played_during_season(no_of_teams):
     correct_input = False
     current_input = None
     while correct_input == False:
         current_input = int(input(f"Enter the number of games played by each team: "))
-        if current_input >= len(teams) - 1:
+        if current_input >= no_of_teams - 1:
             correct_input = True
         else:
             print(
